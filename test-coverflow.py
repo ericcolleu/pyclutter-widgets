50deb3be42e32271da8c435ce29fbcfb4fa2cdb3
eric.colleu <eric.colleu@41bb35fe-fdbe-11de-96e6-c767aa2c8147>
2010-01-22 07:24:55 +0000

    - do my own coverflow

--- test-coverflow.py
+++ test-coverflow.py
@@ -1,79 +1,23 @@
-﻿#!/usr/bin/python
+#!/usr/bin/python
 
-import gobject
 import clutter
 
-from clutter import cogl
-import glob
+import glob, time
 import os.path
 from clutter import keysyms
-from widget.coverflow import HorizontalFlowBrowser
-from widget.reflect import ReflectedItem
+from widget.coverflow import Coverflow
+global current
+current=1
 
-class Coverflow(HorizontalFlowBrowser):
-
-	ITEM_SIZE = (128, 128)
-	CACHED_ITEMS = 8
-	VISIBLE_ITEMS = 3
-
-
-	def __init__(self):
-		self.albums = {}
-		self.filtered = []
-		HorizontalFlowBrowser.__init__(self)
-		self.connect('selection-activated', self.on_activate_item)
-		self.connect('selection-changed', self.on_selection_changed)
-		self.setup_label()
-		self.focused = None
-		self.setup_icons()
-
-	def setup_icons(self):
-		pass
-
-	def setup_label(self):
-		self.label = clutter.Text()
-		self.label.set_color(clutter.color_from_string('WHITE'))
-		self.label.set_use_markup(True)
-		self.label.set_ellipsize(True)
-		self.add(self.label)
-
-	def get_items_number(self):
-		return len(self.filtered)
-
-	def get_item(self, index):
-		album = ReflectedItem(clutter.Texture(self.filtered[index]))
-		album.set_reactive(True)
-		album.connect('button-press-event', lambda i, e: self.focus_item(i.get_parent()))
-		return album
-
-	def on_selection_changed(self, coverflow, index):
-		self.label.set_text('%s' % os.path.basename(self.filtered[index]))
-		self.label.set_position((self.get_width()-self.label.get_width())/2, (self.get_height()-20))
-		self.label.raise_top()
-		if self.focused:
-			self.focused.pop()
-
-	def on_activate_item(self, obj, item):
-		pass
-
-	def clean_item(self, item):
-		for elem in item.get_children()[3:]:
-			item.remove(elem)
-
-	def add_image(self, image):
-		self.filtered.append(image)
-
-	def previous(self):
-		self.selection = (self.selection - 1) % len(self.filtered)
-
-	def next(self):
-		self.selection = (self.selection + 1) % len(self.filtered)
-
-def on_input(stage, event, coverflow):
+def on_input(stage, event, coverflow, item_images):
+	global current
 	if event.keyval == keysyms.Left:
 		coverflow.previous()
 	elif event.keyval == keysyms.Right:
 		coverflow.next()
+	elif event.keyval == keysyms.a:
+		coverflow.add(clutter.Texture(item_images[current]))
+		current = (current + 1) % len(item_images)
 	elif event.keyval == keysyms.q:
 		clutter.main_quit()
 
@@ -81,28 +25,29 @@ def main(image_directory):
 	stage = clutter.Stage()
 	stage.set_size(1024, 768)
 	stage.connect('destroy', clutter.main_quit)
-	stage.set_color(clutter.color_from_string('Black'))
-	stage.set_title('Carousel')
-
-	x, y, width, height = stage.get_width() / 2, stage.get_height() / 2, 200, 200
-	coverflow = Coverflow()
-	#carrousel = clutter.Group()
-	stage.add(coverflow)
-
+	stage.set_color(clutter.Color(0, 0, 0, 255))
+	stage.set_title('Coverflow')
 	item_images = glob.glob(os.path.join(image_directory, "*.png"))
+	x, y, width, height = (
+		int(stage.get_width()/4), 
+		int(stage.get_height()-stage.get_height()/3),
+		int(stage.get_width()/2),
+		int(stage.get_height()-stage.get_height()/4),
+	)
+	
+	coverflow = Coverflow(size=(512, 512), item_size=(128,128))
+	stage.add(coverflow)
 	n_items = len(item_images)
 	items = []
-	coverflow.set_position(0, 768-300)
-	coverflow.set_size(1024, 150)
-	for image in item_images:
-		coverflow.add_image(image)
-
-	coverflow.reload()
-	stage.connect('key-press-event', on_input, coverflow)
 	stage.show()
+	for image in item_images:
+		coverflow.add(clutter.Texture(image))
+	coverflow.set_position(200, 200)
+	stage.connect('key-press-event', on_input, coverflow, item_images)
 	clutter.main()
 
 if __name__ == '__main__':
 	main("./images")
+
 
 
--- widget/coverflow.py
+++ widget/coverflow.py
@@ -1,267 +1,112 @@
 import clutter
-import gobject
-from widget.utils import clamp, clamp_angle
-from widget.animation import MoveAnimation, RotateAnimation, ScaleAnimation, OpacityAnimation
-class FlowBrowser(clutter.Group):
-	"""A base class for items picker views like coverflow, override get_items_number and get_item to use it"""
+from widget.reflect import ReflectedItem
+from widget.animation import MoveAnimation, RotateAnimation, ScaleAnimation, DepthAnimation
+from widget.utils import clamp_angle
 
-	__gsignals__ = {
-		'selection-changed' : ( gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_INT,) ),
-		'selection-activated' : ( gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_OBJECT,) ),
-		'reloaded' : ( gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, () ),
-	}
 
-	CACHED_ITEMS = 12
-	ANIM_DURATION_MS = 200
-
-	def __init__(self):
-		clutter.Group.__init__(self)
-		self._setup_events()
-		self.items = []
-		self.reload()
-
-	def reload(self):
-		self.remove_all_items()
-		self.items = []
-		self._selection = 0
-		self._fill_cache()
-		self.place_items()
-		self.emit('reloaded')
-		self.emit('selection-changed', self.selection)
-
-	def remove_all_items(self):
-		for item in self._cached_items():
-			self.remove(item)
-
-	def _cached_items(self):
-		"""Returns the items in cache"""
-		return filter(None, self.items)
-
-	def _setup_events(self):
-		self.set_reactive(True)
-		self.connect('scroll-event', self.on_scroll)
-
-	def _set_selection(self, selection):
-		"""Change the current selection and reflect change in the view"""
-		if self._selection != selection:
-			if self._selection<selection:
-				move_forward = True
-			else:
-				move_forward = False
-			self._selection = selection
-			self._fill_cache()
-			self.place_items(move_forward)
-			self.emit('selection-changed', selection)
-
-	def _get_selection(self):
-		return self._selection
-
-	selection = property(_get_selection, _set_selection)
-
-	def get_items_number(self):
-		raise NotImplementedError
-
-	def get_item(self, index):
-		raise NotImplementedError
-
-	def add_item(self, item, index=None):
-		"""Add an item to the view"""
-		if index is None: index=len(self.items)
-		self.items.insert(index,item)
-		self.add(item)
-		self.place_new_item(item, index)
-
-	def place_new_item(self, item, index):
-		pass
-
-	def insert_item(self, item, index=None):
-		"""Insert an item and show it immediatly"""
-		if index:
-			if len(self.items)>index and index <= self.selection:
-				self._selection += 1
-		self.add_item(item, index)
-		self.place_items()
-
-	def anim_completed(self, tl):
-		self._reduce_cache()
-
-	def place_item(self, item, move_forward=False):
-		"""Place the item in the view and animate the changes"""
-		index = self.items.index(item)
-		delta = index - self.selection
-
-		anim = self.get_behaviors(item, delta, move_forward, self.alpha)
-		anim.connect("completed", self.anim_completed)
-		anim.start()
-		if self.items[index-1]:
-			if delta>0: item.lower_actor(self.items[index-1])
-			else: item.raise_actor(self.items[index-1])
-
-	def place_items(self, move_forward=False):
-		"""Replace all items in the view according to the current selection index"""
-		self.anim_behaviors = []
-		for item in self._cached_items():
-			self.place_item(item, move_forward)
-
-	def get_behaviors(self, item, delta, move_forward, alpha):
-		"""Returns animations behaviors, basic implementation meant to be overriden
-		"""
-		point = ((self.size[0]-item.get_size()[0])/2+item.get_width()*delta, (self.size[1]-item.get_size()[1])/2)
-		(p_behavior, tl) = animate_actor_to_point(item, point, timeline=self.timeline, alpha=self.alpha)
-		return [p_behavior]
-
-	def _fill_cache(self):
-		"""Fill the cache according to the ITEMS_CACHED value
-		"""
-		start_index = max(self.selection-self.CACHED_ITEMS, 0)
-		end_index = min(self.selection+self.CACHED_ITEMS, self.get_items_number())
-		total_size = self.get_items_number()
-		while(total_size>len(self.items)):
-			self.items.append(None)
-		for i in range(start_index, end_index):
-			if not self.items[i]:
-				del self.items[i]
-				self.add_item(self.get_item(i), i)
-
-	def _reduce_cache(self):
-		"""Reduce the cache and remove unused items from the view"""
-		for i in self._cached_items():
-			index = self.items.index(i)
-			if abs(self.selection-index)>self.CACHED_ITEMS:
-				self.remove(i)
-				self.items[index] = None
-				del i
-
-	def on_scroll(self, fb, event):
-		if event.direction == clutter.SCROLL_DOWN and self.selection>0:
-			self.selection-=1
-		elif event.direction == clutter.SCROLL_UP and self.selection<len(self.items)-1:
-			self.selection+=1
-		return True
-
-	def focus_item(self, item, event=None):
-		if self.items.index(item) == self.selection:
-			self.emit('selection-activated',item)
-		self.selection = self.items.index(item)
-
-class CoverflowItemRotateAnimation(MoveAnimation, RotateAnimation, ScaleAnimation, OpacityAnimation):
-	def __init__(self, destination, angle, axis, direction, scale, opacity, duration, style, timeline=None, alpha=None):
+class CoverflowItemAnimation(MoveAnimation, RotateAnimation, ScaleAnimation, DepthAnimation):
+	def __init__(self, destination, angle, axis, direction, scale, depth, duration, style, timeline=None, alpha=None):
 		MoveAnimation.__init__(self, destination, duration, style, timeline=timeline, alpha=alpha)
 		RotateAnimation.__init__(self, angle, axis, direction, duration, style, timeline=self._timeline, alpha=self._alpha)
 		ScaleAnimation.__init__(self, scale, scale, duration, style, timeline=self._timeline, alpha=self._alpha)
-		OpacityAnimation.__init__(self, opacity, duration, style, timeline=self._timeline, alpha=self._alpha)
+		DepthAnimation.__init__(self, depth, duration, style, timeline=self._timeline, alpha=self._alpha)
 
 	def do_prepare_animation(self):
 		behaviours = MoveAnimation.do_prepare_animation(self)
-		behaviours.extend(RotateAnimation.do_prepare_animation(self))
+		if clamp_angle(self._actor.get_rotation(self._axis)[0]) != clamp_angle(self._angle):
+			behaviours.extend(RotateAnimation.do_prepare_animation(self))
 		behaviours.extend(ScaleAnimation.do_prepare_animation(self))
-		behaviours.extend(OpacityAnimation.do_prepare_animation(self))
+		behaviours.extend(DepthAnimation.do_prepare_animation(self))
 		return behaviours
+
+class Coverflow(clutter.Group):
+	__gtype_name__ = 'Coverflow'
+	def __init__(self, x=0, y=0, size=(512, 128), item_size=(128, 128), *children):
+		clutter.Group.__init__(self)
+		self._x = x
+		self._y = y
+		self._width = size[0]
+		self._height = size[1]
+		self._item_size = item_size
+		self._selected = 0
+		self._inter_item_space = 50
+		self._angle = 70
+		self._selection_depth = 200
+		self._children = []
+		if children:
+			self.add(*children)
+
+	def add(self, *children):
+		[child.set_size(*self._item_size) for child in children]
+		self.do_add(*children)
+		clutter.Group.add(self, *children)
 		
-class CoverflowItemNotRotateAnimation(MoveAnimation, ScaleAnimation, OpacityAnimation):
-	def __init__(self, destination, scale, opacity, duration, style, timeline=None, alpha=None):
-		MoveAnimation.__init__(self, destination, duration, style, timeline=timeline, alpha=alpha)
-		ScaleAnimation.__init__(self, scale, scale, duration, style, timeline=self._timeline, alpha=self._alpha)
-		OpacityAnimation.__init__(self, opacity, duration, style, timeline=self._timeline, alpha=self._alpha)
+	def do_add(self, *children):
+		for child in children:
+			self._children.append(child)
+		self.__update_items_position()
 
-	def do_prepare_animation(self):
-		behaviours = MoveAnimation.do_prepare_animation(self)
-		behaviours.extend(ScaleAnimation.do_prepare_animation(self))
-		behaviours.extend(OpacityAnimation.do_prepare_animation(self))
-		return behaviours
+	def remove(self, *children):
+		clutter.Group.remove(self, *children)
+		self.do_remove(*children)
 		
+	def do_remove(self, *children):
+		for child in children:
+			if child in self._children:
+				self._children.remove(child)
+		self.__update_items_position()
 
-class HorizontalFlowBrowser(FlowBrowser):
-	"""A coverflow like flowbrowser"""
-
-	COVER_DIST=20
-	FRONT_SIZE=100
-	MAX_ANGLE = 70
-	MIN_SCALE = 1
-	MAX_SCALE=1.7
-	VISIBLE_ITEMS = 12
-	FADING_ITEMS = 3
-	ITEM_SIZE = (100, 100)
-
-	def __init__(self):
-		FlowBrowser.__init__(self)
-		self.VISIBLE_ITEMS = self.CACHED_ITEMS
-
-	def refresh_params(self):
-		self.MAX_SCALE = self.get_height()*(1.5)/(self.ITEM_SIZE[1]*2.0)
-		self.MIN_SCALE = self.get_height()/(self.ITEM_SIZE[1]*2.0)
-		self.COVER_DIST = (self.get_width()/2-self.FRONT_SIZE-20)/self.VISIBLE_ITEMS
-
-	def set_size(self, width, height):
-		FlowBrowser.set_size(self, width, height)
-		self.refresh_params()
-		self.place_items()
-
-	def calc_angle(self, delta, move_forward):
-		if delta == 0:
-			if move_forward:
-				rotation_dir = clutter.ROTATE_CW
-			else:
-				rotation_dir = clutter.ROTATE_CCW
-			angle = 0
-
-		if delta>0:
-			rotation_dir = clutter.ROTATE_CCW
-			angle = 360 - self.MAX_ANGLE
-
-		if delta<0:
-			rotation_dir = clutter.ROTATE_CW
-			angle = self.MAX_ANGLE
-
-		return (angle, rotation_dir)
-
-	def calc_pos(self, delta):
-		if (delta == 0):
-			return 0
-		dist = ((abs(delta)-1) * self.COVER_DIST) + self.FRONT_SIZE
-		if delta > 0: return dist
-		else: return -dist
-
-	def calc_scale(self, delta):
-		if delta == 0: return self.MAX_SCALE
-		else: return self.MIN_SCALE
-
-	def calc_opacity(self, delta):
-		delta = abs(delta)
-		if delta < self.VISIBLE_ITEMS-self.FADING_ITEMS:
-			return 255
-		else:
-			delta -= self.VISIBLE_ITEMS-self.FADING_ITEMS
-			return clamp(255*(self.FADING_ITEMS - delta)/self.FADING_ITEMS, 0, 255)
-
-	def place_new_item(self, item, index):
-		delta = index - self.selection
-		if delta>0:
-			item.set_position(self.get_width()+int(item.get_width()*self.MIN_SCALE), (self.get_height()-int(item.get_height()*self.MIN_SCALE))/2)
-		else:
-			item.set_position(-int(item.get_width()*self.MIN_SCALE*2), (self.get_height()-int(item.get_height()*self.MIN_SCALE))/2)
-		item.set_scale(self.MIN_SCALE, self.MIN_SCALE)
-		item.set_opacity(0)
-
-	def get_behaviors(self, item, delta, move_forward, alpha):
-		size = item.get_size()
-		behaviors = []
-
-		if delta==0:
-			point = ((self.get_size()[0]-item.get_size()[0]*self.MAX_SCALE)/2+self.calc_pos(delta), (self.get_size()[1]-item.get_size()[1]*self.MAX_SCALE)/2)
-		else:
-			point = ((self.get_size()[0]-item.get_size()[0]*self.MIN_SCALE)/2+self.calc_pos(delta), (self.get_size()[1]-item.get_size()[1]*self.MIN_SCALE)/2)
-		scale = self.calc_scale(delta)
-		angle_start = item.get_rotation(clutter.Y_AXIS)[0]
-		angle_start = clamp_angle(angle_start)
-		(angle_end, direction) = self.calc_angle(delta, move_forward)
-		if abs(angle_start-angle_end)>1 and abs(angle_start-angle_end)<359:
-			anim = CoverflowItemRotateAnimation(point, angle_end, clutter.Y_AXIS, direction, scale, self.calc_opacity(delta), self.ANIM_DURATION_MS, clutter.EASE_IN_SINE)
-		else:
-			anim = CoverflowItemNotRotateAnimation(point, scale, self.calc_opacity(delta), self.ANIM_DURATION_MS, clutter.EASE_IN_SINE)
+	def __apply_animation(self, item, destination, angle, direction, depth):
+		anim = CoverflowItemAnimation(
+			destination=(destination, self._y), 
+			angle=angle, 
+			axis=clutter.Y_AXIS, 
+			direction=direction, 
+			scale=1.0, 
+			depth=depth,
+			duration=200,
+			style=clutter.EASE_IN_OUT_SINE
+		)
 		anim.apply(item)
-		
 		return anim
-
-
 		
+	def __update_items_position(self, selected_rotation=clutter.ROTATE_CW):
+		center_x = self._x + self._width/2
+		anims=[]
+		print self._selected
+		for index, item in enumerate(self._children[:self._selected]):
+			anims.append(self.__apply_animation(
+				item=item,
+				destination=(center_x - (self._item_size[0]/2) - (self._inter_item_space*((self._selected-index)+1))), 
+				angle=self._angle, 
+				direction=clutter.ROTATE_CW, 
+				depth=0.0))
+			
+		anims.append(self.__apply_animation(
+			item=self._children[self._selected],
+			destination=(center_x - self._item_size[0]/2), 
+			angle=0, 
+			direction=selected_rotation, 
+			depth=self._selection_depth))
+
+		if self._selected < (len(self._children)-1):
+			for index, item in enumerate(self._children[self._selected+1:]):
+				anims.append(self.__apply_animation(
+					item=item,
+					destination=(center_x + (self._item_size[0]/2) + (self._inter_item_space*(index+1))), 
+					angle=360-self._angle, 
+					direction=clutter.ROTATE_CCW, 
+					depth=0.0))
+		[anim.start() for anim in anims]
+
+	def next(self):
+		self._selected = min(self._selected + 1,len(self._children)-1)# % len(self._children)
+		self.__update_items_position(clutter.ROTATE_CW)
+		
+	def previous(self):
+		self._selected = max(self._selected - 1, 0)# % len(self._children)
+		self.__update_items_position(clutter.ROTATE_CCW)
+
+
+
+
