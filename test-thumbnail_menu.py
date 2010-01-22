51db340fe014f6ebdd1441b03ad6972473566f80
eric.colleu <eric.colleu@41bb35fe-fdbe-11de-96e6-c767aa2c8147>
2010-01-22 16:32:48 +0000

    - work in progress : new menu -> thumbnail

---
+++ test-thumbnail_menu.py
@@ -0,0 +1,53 @@
+﻿#!/usr/bin/python
+
+import clutter
+
+import glob, time
+import os.path
+from clutter import keysyms
+from widget.thumbnail_menu import ThumbnailMenu
+global current
+current=1
+
+def on_input(stage, event, thumbnailmenu, item_images):
+	global current
+	if event.keyval == keysyms.Left:
+		thumbnailmenu.previous()
+	elif event.keyval == keysyms.Right:
+		thumbnailmenu.next()
+	elif event.keyval == keysyms.Page_Down:
+		thumbnailmenu.previous_page()
+	elif event.keyval == keysyms.Page_Up:
+		thumbnailmenu.next_page()
+	elif event.keyval == keysyms.a:
+		thumbnailmenu.add(clutter.Texture(item_images[current]))
+		current = (current + 1) % len(item_images)
+	elif event.keyval == keysyms.q:
+		clutter.main_quit()
+
+def main(image_directory):
+	stage = clutter.Stage()
+	stage.set_size(1024,768)
+	stage.connect('destroy', clutter.main_quit)
+	stage.set_color(clutter.Color(0, 0, 0, 255))
+	stage.set_title('Thumbnail Menu')
+	item_images = glob.glob(os.path.join(image_directory, "*.png"))
+	thumbnailmenu = ThumbnailMenu(size=(1024, 512), item_size=(128,128), nb_item_by_page=4)
+	stage.add(thumbnailmenu)
+	n_items = len(item_images)
+	items = []
+	stage.show()
+	for image in item_images:
+		thumbnailmenu.add(clutter.Texture(image))
+	thumbnailmenu.set_position(400, 200)
+	stage.connect('key-press-event', on_input, thumbnailmenu, item_images)
+	clutter.main()
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv[1:]):
+		main(sys.argv[1])
+	else:
+		main("./images")
+
+
---
+++ widget/thumbnail_menu.py
@@ -0,0 +1,76 @@
+﻿import clutter
+from widget.reflect import ReflectedItem
+from widget.animation import MoveAnimation, RotateAnimation, ScaleAnimation, DepthAnimation
+from widget.utils import clamp_angle
+
+
+class ThumbnailItemAnimation(MoveAnimation, OpacityAnimation, DepthAnimation):
+	def __init__(self, destination, opacity, depth, duration, style, timeline=None, alpha=None):
+		MoveAnimation.__init__(self, destination, duration, style, timeline=timeline, alpha=alpha)
+		OpacityAnimation.__init__(self, opacity, duration, style, timeline=self._timeline, alpha=self._alpha)
+		DepthAnimation.__init__(self, depth, duration, style, timeline=self._timeline, alpha=self._alpha)
+
+	def do_prepare_animation(self):
+		behaviours = MoveAnimation.do_prepare_animation(self)
+		behaviours.extend(OpacityAnimation.do_prepare_animation(self))
+		behaviours.extend(DepthAnimation.do_prepare_animation(self))
+		return behaviours
+
+class ThumbnailMenu(clutter.Group):
+	__gtype_name__ = 'ThumbnailMenu'
+	def __init__(self, x=0, y=0, size=(512, 128), item_size=(128, 128), nb_item_by_page=4, selection_depth=200, *children):
+		clutter.Group.__init__(self)
+		self._x = x
+		self._y = y
+		self._width = size[0]
+		self._height = size[1]
+		self._item_size = item_size
+		self._nb_item_by_page = nb_item_by_page
+		self._selected = 0
+		self._selection_depth = selection_depth
+		self._children = []
+		self._pages = [clutter.Group(),]
+		self._current_page = self._pages[0]
+		if children:
+			self.add(*children)
+
+	def add(self, *children):
+		[child.set_size(*self._item_size) for child in children]
+		[child.set_opacity(0) for child in children]
+		self.do_add(*children)
+		clutter.Group.add(self, *children)
+
+	def do_add(self, *children):
+		for child in children:
+			self._children.append(child)
+		self.__update_items_position()
+
+	def remove(self, *children):
+		clutter.Group.remove(self, *children)
+		self.do_remove(*children)
+
+	def do_remove(self, *children):
+		for child in children:
+			if child in self._children:
+				self._children.remove(child)
+		self.__update_items_position()
+
+	def __get_items_in_current_page(self):
+		page_num = int(self._selected / self._nb_item_by_page)
+		return self._children[page_num*self._nb_item_by_page:min((page_num+1)*self._nb_item_by_page, len(self._children))]
+
+	def __update_items_position(self):
+		pass
+
+	def next(self):
+		self._selected = min(self._selected + 1,len(self._children)-1)# % len(self._children)
+		self.__update_items_position()
+
+	def previous(self):
+		self._selected = max(self._selected - 1, 0)# % len(self._children)
+		self.__update_items_position()
+
+
+
+
+
