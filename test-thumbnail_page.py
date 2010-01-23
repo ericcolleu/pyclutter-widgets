7ca29535a53552298f238b052b251edaaa733f13
eric.colleu <eric.colleu@41bb35fe-fdbe-11de-96e6-c767aa2c8147>
2010-01-23 13:33:43 +0000

    - new thumbnail page widget

---
+++ test-thumbnail_page.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import clutter
+
+import glob, time
+import os.path
+from clutter import keysyms
+from widget.thumbnail_menu import ThumbnailPage
+
+def on_input(stage, event, thumbnailpage):
+	if event.keyval == keysyms.Left:
+		thumbnailpage.select((thumbnailpage.get_selected()+1)%4)
+	elif event.keyval == keysyms.Right:
+		thumbnailpage.select((thumbnailpage.get_selected()-1)%4)
+	elif event.keyval == keysyms.q:
+		clutter.main_quit()
+
+def main(image_directory):
+	stage = clutter.Stage()
+	stage.set_size(1024,768)
+	stage.connect('destroy', clutter.main_quit)
+	stage.set_color(clutter.Color(0, 0, 0, 255))
+	stage.set_title('Thumbnail Page')
+	item_images = glob.glob(os.path.join(image_directory, "*.png"))
+	thumbnailpage = ThumbnailPage(size=(1024,768), max_row=5, max_column=5)
+	stage.add(thumbnailpage)
+	n_items = len(item_images)
+	items = []
+	stage.show()
+	for image in item_images:
+		thumbnailpage.add(clutter.Texture(image))
+	thumbnailpage.select(0)
+	stage.connect('key-press-event', on_input, thumbnailpage)
+	clutter.main()
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv[1:]):
+		main(sys.argv[1])
+	else:
+		main("./images")
+
+
+
