126f42a5bebeffcc79b3d974bf9c7d60733026b2
eric.colleu <eric.colleu@41bb35fe-fdbe-11de-96e6-c767aa2c8147>
2010-03-13 09:35:43 +0000

    - added : setup script for package distribution

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='pyclut',
+	version='0.1',
+	description='pyclutter widgets library',
+	author='Eric Colleu',
+	author_email='eric.colleu@gmail.com',
+	url='http://code.google.com/p/pyclutter-widgets/',
+	packages=[
+		'pyclut',
+		'pyclut.basics',
+		'pyclut.controls',
+		'pyclut.effects',
+		'pyclut.effects.transitions',
+		'pyclut.menus',
+	],
+)
+
+
+
