5239d0c307e1a0b15e805543d4d4a10b2fad3bd7
eric.colleu <eric.colleu@41bb35fe-fdbe-11de-96e6-c767aa2c8147>
2010-04-04 08:17:37 +0000

    - added : single tests for animations

---
+++ test-move-animation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import clutter
+from test import PyClutTest
+from clutter import keysyms
+from pyclut.animation import MoveAnimation
+
+class MoveAnimationTest(PyClutTest):
+	def __init__(self, *args, **kwargs):
+		PyClutTest.__init__(self, *args, **kwargs)
+
+	def on_button_press(self, stage, event):
+		anim = MoveAnimation((event.x, event.y), 500, clutter.LINEAR)
+		anim.connect("completed", self._on_done)
+		anim.apply(self.item)
+		anim.start()
+
+	def _on_done(self, event):
+		print "done"
+
+	def run(self):
+		self._stage.connect('key-press-event', self.on_input)
+		self.item = clutter.Texture(self.get_image())
+		self._stage.add(self.item)
+		self._stage.show()
+		self._stage.connect('button-press-event', self.on_button_press)
+		clutter.main()
+
+if __name__ == '__main__':
+	test = MoveAnimationTest()
+	test.run()
+
+
+
