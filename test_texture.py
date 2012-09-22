d1b0c24ebf1d593f187f62aa198677108adbd11c
eric.colleu <eric.colleu@41bb35fe-fdbe-11de-96e6-c767aa2c8147>
2012-09-22 19:43:14 +0000

    fix texture example for clutter 1.10

--- test_texture.py
+++ test_texture.py
@@ -2,14 +2,18 @@
 import sys
 from gi.repository import Clutter
 
+def do_quit(*args):
+	Clutter.main_quit()
+
+Clutter.init(sys.argv)
 stage = Clutter.Stage()
 stage.set_size(1024,768)
-stage.connect('destroy', Clutter.main_quit)
-stage.set_color(Clutter.Color(0, 0, 0, 255))
+stage.connect('destroy', do_quit)
+stage.set_color(Clutter.Color.new(0, 0, 0, 255))
 stage.set_title('texture')
 
 stage.show()
-texture = Clutter.Texture(sys.argv[0])
+texture = Clutter.Texture.new_from_file(sys.argv[1])
 stage.add_actor(texture)
 
 #print texture.get_cogl_texture()
